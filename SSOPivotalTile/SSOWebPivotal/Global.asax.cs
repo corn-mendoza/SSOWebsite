5fb6b503efd3aecfaf0cc610f48704aa023fba2a
Corn Mendoza <cmendoza@pivotal.io>
2017-06-15 14:38:46 -0500

    Updated packages

--- SSOPivotalTile/SSOWebPivotal/Global.asax.cs
+++ SSOPivotalTile/SSOWebPivotal/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,9 @@ namespace SSOWebPivotal
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+
         }
     }
 }
